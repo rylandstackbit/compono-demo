495868e2a71028f0282bc9d44eaef7e07f9d0096
Stackbit Code Editor <projects@stackbit.com>
2021-08-11 16:42:02 +0000

    BlogPostFooter.js: changed

--- src/components/BlogPostFooter.js
+++ src/components/BlogPostFooter.js
@@ -13,7 +13,7 @@ export default function BlogPostFooter(props) {
     let footerTextColor = _.get(post, 'footerTextColor');
     
     return (
-        <footer className="post-meta" style={{color: footerTextColor, backgroundColor: `coral`}}>
+        <footer className="post-meta" style={{color: footerTextColor}}>
             {/* <time className="published" dateTime={dateTimeAttr}>
                 {formattedDate}
             </time>
