d1a79b7ecf39daf8544e3f360fa265c26b36b16a
Stackbit Code Editor <projects@stackbit.com>
2021-08-11 16:30:12 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -64,7 +64,7 @@ export default function SectionHero(props) {
     } else if (textBlockWidth === "medium") {
         textBlockWidth = "71%";
     } else if (textBlockWidth === "full") {
-        textBlockWidth = "100%";
+        textBlockWidth = "20rem";
     }
     
     return (
